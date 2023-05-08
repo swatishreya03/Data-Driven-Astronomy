35c5352110851fcb2bd49ec317984b993bf6e56e
Swati Shreya <shreyaswati65@gmail.com>
2023-05-09 02:36:54 +0530

    DDA file

---
+++ Week 1/mean.py
@@ -0,0 +1,7 @@
+def calculate_mean(mean):
+  return sum(mean)/len(mean) 
+
+if __name__ == '__main__':
+  # Run your `calculate_mean` function with examples:
+  mean = calculate_mean([1,2.2,0.3,3.4,7.9])
+  print(mean)
